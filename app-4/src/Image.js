ad0bd3e2766e9d2ea53702cdbfd592dc4fad6337
Bao Le <jbvhl@hotmail.com>
2019-02-04 14:55:20 -0700

    done

--- app-4/src/Image.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Image(props) {
-    return (
-        <div>
-            <img src={props.myImage} alt ='https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.fit-760w.jpg'/> 
-        </div>
-    )
-}
